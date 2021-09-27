0c20a7788f95c5e5c9f6c0e1d8ff561597d0f531
aman dangol <amandangol423@gmail.com>
2021-09-27 19:30:19 +0545

    test

---
+++ aman.py
@@ -0,0 +1,10 @@
+import csv
+import json
+
+csvfile = open('D:\drowsy\python_env\FaceTrainDubey\StudentDetails\StudentDetails.csv', 'r')
+jsonfile = open('file.json', 'w')
+
+fieldnames = ("Id","Name","mail","pnumber")
+reader = csv.DictReader( csvfile, fieldnames)
+out = json.dumps( [ row for row in reader ] )
+jsonfile.write(out)
